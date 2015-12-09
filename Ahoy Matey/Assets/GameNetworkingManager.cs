d82173c0f69a249ac0a7ecccfc138ecd4ffed3d9
Sean Harrison <seanharrs97@hotmail.com>
2015-12-09 23:35:58 +1000

    Created a custom network manager script

---
+++ Ahoy Matey/Assets/GameNetworkingManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class GameNetworkingManager : NetworkManager
+{
+	public void StartGameHost()
+	{
+		StartHost();
+		Debug.Log("Starting: " + Time.timeSinceLevelLoad);
+		FindObjectOfType<Button>().gameObject.SetActive(false);
+	}
+	
+	public override void OnStartHost()
+	{
+		base.OnStartHost();
+		Debug.Log("Started: " + Time.timeSinceLevelLoad);
+	}
+}
