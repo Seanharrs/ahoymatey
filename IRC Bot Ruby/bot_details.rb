842fd50d8334c8cb21eadfd92e4683e592506d69
Sean Harrison <seanharrs97@hotmail.com>
2016-02-26 15:22:40 +1000

    -change style of code in ruby bot

--- IRC Bot Ruby/bot_details.rb
+++ IRC Bot Ruby/bot_details.rb
@@ -1,5 +1,7 @@
 class BotDetails
-  def initialize(real, user, host, nick, server, channel, port)
+  attr_reader :real, :user, :host, :nick, :server, :channel, :port, :admin_names
+
+  def initialize real, user, host, nick, server, channel, port
     @real = real
     @user = user
     @host = host
@@ -12,37 +14,5 @@ class BotDetails
                     "AusBotCSharp", "Aus_Bot_CSharp",
                     "AusBotFSharp", "Aus_Bot_FSharp",
                     "AusBotCPlusPlus", "Aus_Bot_CPlusPlus"]
-  end
-  
-  def get_realname
-    @real
-  end
-  
-  def get_username
-    @user
-  end
-  
-  def get_nickname
-    @nick
-  end
-  
-  def get_host
-    @host
-  end
-  
-  def get_server
-    @server
-  end
-  
-  def get_channel
-    @channel
-  end
-  
-  def get_port
-    @port
-  end
-  
-  def get_admin_names
-    @admin_names
   end
 end
--- IRC Bot Ruby/irc_bot.rb
+++ IRC Bot Ruby/irc_bot.rb
@@ -2,29 +2,28 @@ require_relative 'bot_details'
 require 'socket'
 
 class IrcBot
-  def initialize(real, user, host, nick, server, channel, port = 6667)
-    @details = BotDetails.new(real, user, host, nick, server, channel, port)
+  def initialize real, user, host, nick, server, channel, port = 6667
+    @details = BotDetails.new real, user, host, nick, server, channel, port
   end
-  
+
   def run
     begin
       connect
-      
-      send_data("NICK", @details.get_nickname)
-      user = "#{@details.get_username} #{@details.get_host} " +
-             "#{@details.get_server} :#{@details.get_realname}"
-      sleep(1)
-      send_data("USER", user)
-      while true do
-        line = ""
+
+      send_data "NICK", @details.nick
+      user = "#{@details.user} #{@details.host} " +
+             "#{@details.server} :#{@details.real}"
+      sleep 1
+      send_data "USER", user
+      loop do
         count = 0
         until (line = @irc.gets) == nil
-          send_data("JOIN", @details.get_channel) if count == 4
-          case check_server_messages(line)
+          send_data "JOIN", @details.channel if count == 4
+          case check_server_messages line
           when :continue then next
           when :quit then return
           end
-          return if check_user_messages(line) == :quit
+          return if (check_user_messages line) == :quit
           count += 1
         end
       end
@@ -32,67 +31,69 @@ class IrcBot
       disconnect
       puts "An exception occurred!\n\n#{e.message}"
       puts "Backtrace: #{e.backtrace.inspect}"
-      sleep(10)
+      sleep 10
       raise
       run
     ensure disconnect
     end
   end
-  
+
   private
-  
+
   def connect
-    @irc = TCPSocket.new(@details.get_server, @details.get_port)
+    @irc = TCPSocket.new @details.server, @details.port
   end
-  
-  def send_data(command, param)
+
+  def send_data command, param
     puts "#{command} #{param}"
-    @irc.puts("#{command} #{param}")
+    @irc.puts "#{command} #{param}"
     @irc.flush
   end
-  
-  def check_server_messages(line)
-    if (line.include? "PING")
+
+  def check_server_messages line
+    if line.include? "PING"
       puts line
-      send_data("PONG", line.split(" ").at(1))
-    elsif (line.include? "JOIN")
+      send_data "PONG", line.split(" ")[1]
+    elsif line.include? "JOIN"
       puts line
       if (m = line.match(/:([^!]*)!/))
         name = m.captures[0]
-        try_op(name) if (@details.get_admin_names.include? name)
-        send_data("PRIVMSG", "#{@details.get_channel} :Hello, #{name}") if !(name == @details.get_nickname)
+        try_op name if @details.admin_names.include? name
+        msg = "#{@details.channel} :Hello, #{name}"
+        send_data "PRIVMSG", msg unless name == @details.nick
       end
     else return :do_nothing
     end
-    return :continue
+    :continue
   end
-  
-  def check_user_messages(line)
-    return :quit if (line.include? ".botquit")
-    
-    if (line.include? ".get")
+
+  def check_user_messages line
+    return :quit if line.include? ".botquit"
+
+    if line.include? ".get"
       puts line
       m = line.match(/:([^!]*)!.*?\.get ([\S ]*)/)
       name, query = m.captures
-      query.sub!(" ", "_")
+      query.sub! " ", "_"
       url = "https://en.wikipedia.org/wiki/" + query.downcase
-      send_data("PRIVMSG", "#{@details.get_channel} :#{name}: #{url}")
-    elsif (line.include? "MODE") && !(line.include? "PRIVMSG")
+      msg = "#{@details.channel} :#{name}: #{url}"
+      send_data "PRIVMSG", msg
+    elsif line.include?("MODE") && !line.include?("PRIVMSG")
       puts line
-      send_data("JOIN", @details.get_channel)
+      send_data "JOIN", @details.channel
       if (m = line.match(/-o (\S*)/))
         name = m.captures[0]
-        try_op(name) if (@details.get_admin_names.include? name)
+        try_op name if @details.admin_names.include? name
       end
     else return :do_nothing
     end
     return :continue
   end
-  
-  def try_op(name)
-    send_data("MODE", "#{@details.get_channel} +o #{name}")
+
+  def try_op name
+    send_data "MODE", "#{@details.channel} +o #{name}"
   end
-  
+
   def disconnect
     @irc.close
   end
